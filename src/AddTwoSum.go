7a1d473ab8e61f6d19bb802d2b2c4755b3f6eaa4
lx <lxstormy@gmail.com>
2018-02-01 22:42:43 +0800

    improve add two sum code

--- src/AddTwoSum.go
+++ src/AddTwoSum.go
@@ -6,45 +6,35 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    res := new(ListNode)
-    res.Next = nil
-    p1, p2 := l1, l2
-    cur := res
-    carry := 0
+	carry, val1, val2, sum, remainder := 0, 0, 0, 0, 0
+	p1, p2 := l1, l2
+	res := ListNode{Val: 0, Next: nil}
+	p3 := &res
 
-    for ; p1 != nil || p2 != nil; {
-        tmpNode := new(ListNode)
-        var v1, v2 int
+	for p1 != nil || p2 != nil || carry != 0 {
+		node := new(ListNode)
+		val1, val2 = 0, 0
+		if p1 != nil {
+			val1 = p1.Val
+		}
+		if p2 != nil {
+			val2 = p2.Val
+		}
+		sum = val1 + val2 + carry
+		carry = sum / 10
+		remainder = sum % 10
 
-        if p1 != nil {
-            v1 = p1.Val
-        }
-        if p2 != nil {
-            v2 = p2.Val
-        }
-        sum := v1 + v2 + carry
-        tmpNode.Val = sum % 10
-        tmpNode.Next = nil
-        carry = sum / 10
-
-        cur.Next = tmpNode
-        cur = tmpNode
-
-        if p1 != nil {
-            p1 = p1.Next
-        }
-        if p2 != nil {
-            p2 = p2.Next
-        }
-    }
-
-    if carry != 0 {
-        tmpNode := new(ListNode)
-        tmpNode.Val = 1
-        tmpNode.Next = nil
-        cur.Next = tmpNode
-    }
-
-    return res.Next
-
+		node.Val = remainder
+		node.Next = nil
+		p3.Next = node
+		p3 = node
+		if p1 != nil {
+			p1 = p1.Next
+		}
+		if p2 != nil {
+			p2 = p2.Next
+		}
+	}
+	return res.Next
 }
+
