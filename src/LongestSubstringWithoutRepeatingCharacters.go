3d321306d7e6b32edaae43f7ea375657a228f553
lx <lxstormy@gmail.com>
2018-02-02 01:04:47 +0800

    add solution

--- src/LongestSubstringWithoutRepeatingCharacters.go
+++ src/LongestSubstringWithoutRepeatingCharacters.go
@@ -1,18 +1,40 @@
 func lengthOfLongestSubstring(s string) int {
-    start, end, maxLen := 0, 0, 0
-    m := map[byte]int{}
+	start, end, maxLen := 0, 0, 0
+	m := map[byte]int{}
 
-    for ; end < len(s); end++ {
-        id, ok := m[s[end]]
-        if ok && id >= start {
-            start = id + 1
-        }
-        m[s[end]] = end
-        curLen := end - start + 1
-        if curLen > maxLen {
-            maxLen = curLen
-        }
-    }
+	for ; end < len(s); end++ {
+		id, ok := m[s[end]]
+		if ok && id >= start {
+			start = id + 1
+		}
+		m[s[end]] = end
+		curLen := end - start + 1
+		if curLen > maxLen {
+			maxLen = curLen
+		}
+	}
 
-    return maxLen
+	return maxLen
 }
+
+//2018.2.2 rewrite
+//func lengthOfLongestSubstring(s string) int {
+//slen := len(s)
+//m := make(map[byte]int)
+//left, right, maxLen := 0, 0, 0
+
+//for ; right < slen; right++ {
+//curChar := s[right]
+//lastPos, ok := m[curChar]
+//m[curChar] = right
+//if ok && lastPos >= left {
+//left = lastPos + 1
+//continue
+//}
+//l := right - left + 1
+//if l > maxLen {
+//maxLen = l
+//}
+//}
+//return maxLen
+//}
