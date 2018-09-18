98999affc4095aa4907a18421294ef1a7439efdd
lx <lxstormy@gmail.com>
2018-09-18 16:21:34 +0800

    decode ways

---
+++ src/DecodeWays/decode_ways.go
@@ -0,0 +1,81 @@
+package DecodeWays
+
+import (
+	"strconv"
+)
+
+// DP solution
+func numDecodings(s string) int {
+	isValidAplhabet := func(numStr string) bool {
+		if len(numStr) == 0 || len(numStr) > 2 {
+			return false
+		}
+		if len(numStr) == 1 {
+			return numStr[0] != '0'
+		}
+		return numStr[0] == '1' || (numStr[0] == '2' && numStr[1] < '7')
+	}
+
+	if len(s) == 0 {
+		return 0
+	}
+	if len(s) == 1 {
+		if isValidAplhabet(s[0:1]) {
+			return 1
+		}
+		return 0
+	}
+	mem := make([]int, len(s))
+	if !isValidAplhabet(s[0:1]) {
+		return 0
+	}
+	mem[0] = 1
+	for i := 1; i < len(s); i++ {
+		if isValidAplhabet(s[i : i+1]) {
+			mem[i] += mem[i-1]
+		}
+		if isValidAplhabet(s[i-1 : i+1]) {
+			if i == 1 {
+				mem[i]++
+			} else {
+				mem[i] += mem[i-2]
+			}
+		}
+	}
+
+	return mem[len(s)-1]
+}
+
+// too slow
+func numDecodingsBacktrack(s string) int {
+	isValidAplhabet := func(numStr string) bool {
+		n, err := strconv.Atoi(numStr)
+		if err != nil {
+			return false
+		}
+		if n >= 1 && n <= 26 {
+			return true
+		}
+		return false
+	}
+	l := len(s)
+	count := 0
+	var helper func(begin int)
+	helper = func(begin int) {
+		if begin == l {
+			count++
+			return
+		}
+		for i := begin; i < l; i++ {
+			curPart := s[begin : i+1]
+			if isValidAplhabet(curPart) {
+				helper(i + 1)
+			} else {
+				break
+			}
+		}
+	}
+	helper(0)
+	return count
+
+}
--- src/DecodeWays/decode_ways_test.go
+++ src/DecodeWays/decode_ways_test.go
@@ -1,1 +1,22 @@
 package DecodeWays
+
+import "testing"
+
+type testCase struct {
+	input string
+	ans   int
+}
+
+func TestF(t *testing.T) {
+	testCases := []testCase{
+		testCase{"12", 2},
+		testCase{"2261", 3},
+		testCase{"799", 1},
+	}
+	for _, curCase := range testCases {
+		result := numDecodings(curCase.input)
+		if result != curCase.ans {
+			t.Errorf("Test case is %v, result is %v", curCase, result)
+		}
+	}
+}
